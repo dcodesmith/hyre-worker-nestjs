ae9d06876769814730c236a233d6736844ee53d4
Afees Kolawole <dcodesmith@gmail.com>
2025-09-04 14:43:02 +0100

    add basic file and test for setup

---
+++ src/main.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { main } from "./main";
+
+describe("main", () => {
+  it("should be defined", () => {
+    expect(main).toBeDefined();
+  });
+});
