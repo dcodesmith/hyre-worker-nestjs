4b5d913d5560c78f84b441763640b084853b037f
Afees Kolawole <dcodesmith@gmail.com>
2025-09-04 14:28:27 +0100

    add ts and vitest config

---
+++ vitest.config.mjs
@@ -0,0 +1,55 @@
+import { resolve } from "node:path";
+import swc from "unplugin-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["src/**/*.{test,spec}.ts"],
+    exclude: ["node_modules/**", "dist/**"],
+    setupFiles: ["./test/setup.ts"],
+    pool: "threads",
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
+  },
+  coverage: {
+    provider: "v8",
+    reporter: ["text", "lcov", "html"],
+    reportsDirectory: "./coverage",
+    exclude: [
+      "node_modules/",
+      "dist/",
+      "build/",
+      "test/",
+      "**/*.spec.ts",
+      "**/*.test.ts",
+      "**/types/",
+      "**/dto/",
+      "**/*.interface.ts",
+      "**/*.module.ts",
+      "src/main.ts",
+      "prisma/",
+      "cucumber.cjs",
+      "vitest.config.mjs",
+    ],
+    include: ["src/**/*.ts"],
+    all: true,
+  },
+  plugins: [
+    tsconfigPaths(),
+    swc.vite({
+      module: { type: "es6" },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "~": resolve(__dirname, "./"),
+    },
+  },
+});
