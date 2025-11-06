355e87e740d2d2eb58c99ab2dd37f150efbc858e
Afees Kolawole <dcodesmith@gmail.com>
2025-11-06 17:10:33 +0000

    (feat): import module, dont register queue again

--- src/modules/job/job.module.ts
+++ src/modules/job/job.module.ts
@@ -1,13 +1,17 @@
-import { BullModule } from "@nestjs/bullmq";
+// import { BullModule } from "@nestjs/bullmq";
 import { Module } from "@nestjs/common";
-import { REMINDERS_QUEUE, STATUS_UPDATES_QUEUE } from "../../config/constants";
+import { ReminderModule } from "../reminder/reminder.module";
+import { StatusChangeModule } from "../status-change/status-change.module";
+// import { REMINDERS_QUEUE, STATUS_UPDATES_QUEUE } from "../../config/constants";
 import { JobController } from "./job.controller";
 import { JobService } from "./job.service";
 
 @Module({
   imports: [
-    BullModule.registerQueue({ name: REMINDERS_QUEUE }),
-    BullModule.registerQueue({ name: STATUS_UPDATES_QUEUE }),
+    // BullModule.registerQueue({ name: REMINDERS_QUEUE }),
+    // BullModule.registerQueue({ name: STATUS_UPDATES_QUEUE }),
+    ReminderModule,
+    StatusChangeModule,
   ],
   controllers: [JobController],
   providers: [JobService],
